686717142fcb0fc0ded28075923ad1a2592d4401
MateusJFabricio <fabriciomateus05@gmail.com>
2021-11-17 16:00:23 -0300

    Corrigido a versao

--- NavegaTeus/Properties/AssemblyInfo.cs
+++ NavegaTeus/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // conjunto de atributos. Altere estes valores de atributo para modificar as informações
 // associadas a um assembly.
 [assembly: AssemblyTitle("NavegaTeus")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Navegador batuta")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NavegaTeus")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
 // usando o "*" como mostrado abaixo:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.1")]
+[assembly: AssemblyFileVersion("3.0.0.1")]
